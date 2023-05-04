1f16052bd9641293591e18432458a7be2481645e
tmax818 <tylermaxwell661@gmail.com>
2023-05-04 15:28:14 -0600

    fin sol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
+  const [pokemon,  setPokemon] = useState([])
+  const getPokemon = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=900")
+        .then(res => {
+          console.log(res.data.results)
+          setPokemon(res.data.results)
+        })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getPokemon}>Fetch Pokemon</button>
+        <ul>
+
+        {pokemon.map(p => (
+            <li>{p.name}</li>
+        ))}
+        </ul>
     </div>
   );
 }
